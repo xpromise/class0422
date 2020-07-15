f2c3918e1e46a40186e00f7354726eec8e261bd3
xiongjian <webjsforyou@gmail.com>
2020-07-15 14:32:21 +0800

    feat: session的使用

--- 01.nodejs/day06/03.session/server.js
+++ 01.nodejs/day06/03.session/server.js
@@ -20,6 +20,11 @@ const http = require("http");
 
     2. session
 
+      本质上是存储在服务器对象
+      大小和数量理论上没有限制
+      传输流量小（只有一个cookie）
+      安全性较高
+
 */
 // 创建session对象
 const session = {};
@@ -34,10 +39,66 @@ function uniqueId() {
   return (randomNum + Date.now()).toString(32);
 }
 
-const server = http.createServer((req, res) => {
+/*
+  如果访问 /login 代表要存储用户信息
+  如果访问 /user 代表要获取用户信息
+*/
 
-  
-  res.end("success");
+const server = http.createServer((req, res) => {
+  // console.log(req.url); // /?name=jack&age=18
+  // name=jack&age=18
+  const url = req.url;
+  if (url === "/favicon.ico") return;
+
+  if (url.startsWith("/login")) {
+    // 获取查询字符串参数并解析成对象
+    const qs = url.split("?")[1];
+    // ['name=jack', 'age=18']
+    const arr = qs.split("&");
+    const query = arr.reduce((p, item) => {
+      const [key, value] = item.split("=");
+      p[key] = value;
+      return p;
+    }, {});
+
+    // console.log(query); // { name: 'jack', age: '18' }
+    // 创建session_id
+    const sessionId = uniqueId();
+    // 将用户数据存到session对象中
+    session[sessionId] = query;
+    // 返回一个携带session_id的cookie给用户
+    res.setHeader(
+      "Set-Cookie",
+      `sessionId=${sessionId};max-age=3600;httpOnly=true`
+    );
+
+    res.end("success");
+
+    return;
+  }
+
+  if (url === "/user") {
+    // 获取请求头cookie并解析成对象
+    const cookies = req.headers.cookie.split("; ").reduce((p, c) => {
+      const [key, value] = c.split("=");
+      p[key] = value;
+      return p;
+    }, {});
+    // 找到sessionId
+    const { sessionId } = cookies;
+    // 找到用户数据
+    const user = session[sessionId];
+
+    if (user) {
+      // 说明用户上次访问过/login
+      res.setHeader("Content-Type", "application/json");
+      res.end(JSON.stringify(user));
+      return;
+    }
+
+    res.setHeader("Content-Type", "text/plain;charset=utf8");
+    res.end("暂无数据~");
+  }
 });
 
 server.listen(9527, "localhost", (err) => {
