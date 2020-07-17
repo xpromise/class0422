b04650b8243f9368c78ae9c3f49e1ebb37b8755b
xiongjian <webjsforyou@gmail.com>
2020-07-17 15:50:58 +0800

    feat: cors

--- 01.nodejs/day07/server.js
+++ 01.nodejs/day07/server.js
@@ -11,7 +11,49 @@ http
       return;
     }
 
+    if (req.url === "/cors") {
+      // CORS方案
+      const safeUrl = [
+        "http://127.0.0.1:5500",
+        "http://127.0.0.1:3000",
+        "http://127.0.0.1:4000",
+      ];
+      // 请求来源地址
+      const origin = req.headers.origin;
+
+      const currentUrl = safeUrl.find((url) => url === origin);
+
+      /*
+        OPTIONS：预检请求
+          作用：检查当前请求是否可以跨域
+      */
+      if (currentUrl) {
+        // 允许哪些地址可以跨域
+        res.setHeader("Access-Control-Allow-Origin", currentUrl);
+        // 允许哪些请求头可以跨域
+        res.setHeader("Access-Control-Allow-Headers", "bbb, auth");
+        // 允许哪些请求方式可以跨域
+        res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        // 预检请求缓存时间
+        res.setHeader("Access-Control-Max-Age", 3600 * 24);
+      }
+      // 允许所有地址跨域
+      // res.setHeader("Access-Control-Allow-Origin", "*");
+      // 允许一个地址跨域
+      // res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
+
+      res.setHeader("content-type", "application/json");
+
+      const person = {
+        name: "jack",
+        age: 18,
+      };
+      res.end(JSON.stringify(person));
+      return;
+    }
+
     if (req.method === "GET") {
+      // JSONP方案
       // console.log(req.url); // 请求的查询字符串参数 /?name=jack&age=18
       // res.end("success4444~");
       const { callback } = req.url
@@ -23,13 +65,13 @@ http
           return p;
         }, {});
 
-        const person = {
-          name: 'jack',
-          age: 18
-        }
-        res.setHeader('content-type', 'applicaion/javascript');
-        // getData(JSON.stringify(person))
-        res.end(`${callback}(${JSON.stringify(person)})`);
+      const person = {
+        name: "jack",
+        age: 18,
+      };
+      res.setHeader("content-type", "application/javascript");
+      // getData(JSON.stringify(person))
+      res.end(`${callback}(${JSON.stringify(person)})`);
     }
 
     if (req.method === "POST") {
