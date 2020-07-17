f5a23b39626ce75aa7092ca42889be7d65beac10
xiongjian <webjsforyou@gmail.com>
2020-07-17 10:52:07 +0800

    feat: ajax发送GET和POST请求

--- 01.nodejs/day07/server.js
+++ 01.nodejs/day07/server.js
@@ -1,14 +1,29 @@
-const http =require('http');
-const fs = require('fs');
+const http = require("http");
+const fs = require("fs");
 
-http.createServer((req, res) => {
-  console.log('服务器接收到请求了~');
-  if (req.url === '/ajax.html') {
-    const rs = fs.createReadStream('./01.ajax.html');
-    res.setHeader('Content-Type', 'text/html;charset=utf8');
-    rs.pipe(res);
-    return;
-  }
+http
+  .createServer((req, res) => {
+    console.log("服务器接收到请求了~");
+    if (req.url === "/ajax.html") {
+      const rs = fs.createReadStream("./02.ajax.html");
+      res.setHeader("Content-Type", "text/html;charset=utf8");
+      rs.pipe(res);
+      return;
+    }
 
-  res.end('success~');
-}).listen(9527)
+    if (req.method === "GET") {
+      console.log(req.url); // 请求的查询字符串参数 /?name=jack&age=18
+      res.end("success4444~");
+    }
+
+    if (req.method === "POST") {
+      // 接收报文体数据
+      req.on('data', (chunk) => {
+        // console.log(chunk.toString()); // name=jack&age=18
+        console.log(chunk.toString()); // {"name":"jack","age":20}
+      })
+
+      res.end("success4444~");
+    }
+  })
+  .listen(9527);
