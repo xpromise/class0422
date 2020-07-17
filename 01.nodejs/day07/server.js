0226b45f6dbd91b06009a0d6e1aadfa63bf30568
xiongjian <webjsforyou@gmail.com>
2020-07-17 11:52:55 +0800

    feat: jquery和axios发送请求

--- 01.nodejs/day07/server.js
+++ 01.nodejs/day07/server.js
@@ -5,7 +5,7 @@ http
   .createServer((req, res) => {
     console.log("服务器接收到请求了~");
     if (req.url === "/ajax.html") {
-      const rs = fs.createReadStream("./02.ajax.html");
+      const rs = fs.createReadStream("./04.ajax.html");
       res.setHeader("Content-Type", "text/html;charset=utf8");
       rs.pipe(res);
       return;
@@ -18,10 +18,10 @@ http
 
     if (req.method === "POST") {
       // 接收报文体数据
-      req.on('data', (chunk) => {
+      req.on("data", (chunk) => {
         // console.log(chunk.toString()); // name=jack&age=18
         console.log(chunk.toString()); // {"name":"jack","age":20}
-      })
+      });
 
       res.end("success4444~");
     }
