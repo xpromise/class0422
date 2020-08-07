f3ea603f4947e62100cfab94d53a8ea5c4d1a06c
xiongjian <webjsforyou@gmail.com>
2020-08-07 15:40:57 +0800

    feat: 数据代理注释

--- 04.vue/04.VueSource/js/mvvm.js
+++ 04.vue/04.VueSource/js/mvvm.js
@@ -1,54 +1,73 @@
+/**
+ * 构造函数，相当于vue
+ * @param {*} options 配置对象
+ */
 function MVVM(options) {
-    this.$options = options || {};
-    var data = this._data = this.$options.data;
-    var me = this;
+  // 给实例对象vm添加$options，值是配置对象
+  this.$options = options || {};
+  // 给实例对象vm添加_data（原数据），值就是配置对象中data数据
+  // 定义变量data，值就是配置对象中data数据
+  var data = this._data = this.$options.data;
+  // 缓存this，为了后面函数可以使用
+  var me = this;
 
-    // 数据代理
-    // 实现 vm.xxx -> vm._data.xxx
-    Object.keys(data).forEach(function(key) {
-        me._proxyData(key);
-    });
+  // 数据代理
+  // 遍历data数据提取所有key，对其数据代理
+  Object.keys(data).forEach(function (key) {
+    // 数据代理的方法
+    me._proxyData(key);
+  });
 
-    this._initComputed();
+  // 代理计算属性
+  this._initComputed();
 
-    observe(data, this);
+  observe(data, this);
 
-    this.$compile = new Compile(options.el || document.body, this)
+  this.$compile = new Compile(options.el || document.body, this);
 }
 
+// 构造函数的原型对象
 MVVM.prototype = {
-    constructor: MVVM,
-    $watch: function(key, cb, options) {
-        new Watcher(this, key, cb);
-    },
+  constructor: MVVM,
+  $watch: function (key, cb, options) {
+    new Watcher(this, key, cb);
+  },
+  // 数据代理的方法：将data数据代理到vm上
+  _proxyData: function (key, setter, getter) {
+    // this._proxyData(key) 所以_proxyData的this指向vm
+    var me = this;
+    setter =
+      setter ||
+      // 将data数据代理到vm上
+      Object.defineProperty(me, key, {
+        configurable: false, // 不能重新配置和删除 
+        enumerable: true, // 可以被枚举
+        get: function proxyGetter() {
+          // 代理属性的读方法
+          // 实际上返回是原数据的值
+          return me._data[key];
+        },
+        set: function proxySetter(newVal) {
+          // 代理属性的写方法
+          // 实际上更新原数据的值
+          me._data[key] = newVal;
+        },
+      });
+  },
 
-    _proxyData: function(key, setter, getter) {
-        var me = this;
-        setter = setter || 
+  _initComputed: function () {
+    var me = this;
+    var computed = this.$options.computed;
+    if (typeof computed === "object") {
+      Object.keys(computed).forEach(function (key) {
         Object.defineProperty(me, key, {
-            configurable: false,
-            enumerable: true,
-            get: function proxyGetter() {
-                return me._data[key];
-            },
-            set: function proxySetter(newVal) {
-                me._data[key] = newVal;
-            }
+          get:
+            typeof computed[key] === "function"
+              ? computed[key]
+              : computed[key].get,
+          set: function () {},
         });
-    },
-
-    _initComputed: function() {
-        var me = this;
-        var computed = this.$options.computed;
-        if (typeof computed === 'object') {
-            Object.keys(computed).forEach(function(key) {
-                Object.defineProperty(me, key, {
-                    get: typeof computed[key] === 'function' 
-                            ? computed[key] 
-                            : computed[key].get,
-                    set: function() {}
-                });
-            });
-        }
+      });
     }
+  },
 };
