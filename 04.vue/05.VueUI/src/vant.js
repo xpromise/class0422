010fdd98b2fab9c7cf04d4e106e244084a7f626d
xiongjian <webjsforyou@gmail.com>
2020-08-10 14:52:43 +0800

    feat: Vue脚手架的介绍和基本使用

--- 04.vue/05.VueUI/src/vant.js
+++ 04.vue/05.VueUI/src/vant.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import { Button, Notify, Swipe, SwipeItem } from "vant";
 
+// 注册组件
 Vue.use(Button);
 Vue.use(Notify);
 Vue.use(Swipe);
---
+++ 04.vue/06.VueCli/01.vue-cli3-4/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <h1>hello vue cli</h1>
+    <img alt="Vue logo" src="../assets/logo.png" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue';
+
+export default {
+  name: 'Home',
+  // components: {
+  //   HelloWorld,
+  // },
+};
+</script>
